e48ae53b6c0a09908726a8ac4c4c59d83e7874e5
sagun1151 <sshrestha426699@gmail.com>
2025-11-17 21:23:56 -0700

    downloaded bootstrap added in view

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio_app',
+    'django_bootstrap5',
 ]
 
 AUTHENTICATION_BACKENDS = [
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+
 # Create your views here.
 def index(request):
-
+   student_active_portfolios = Student.objects.select_related('portfolio').all().filter(Portfolio__is_active=True)
+   print('active portfolio query set', student_active_portfolios)
 
 # Render the HTML template index.html with the data in the context variable.
-   return render( request, 'portfolio_app/index.html')
+   return render(request, 'portfolio_app/index.html', {'student_active_portfolios': student_active_portfolios})
